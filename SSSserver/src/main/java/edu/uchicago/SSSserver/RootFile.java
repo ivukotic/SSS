70ba643519bbce475d27ce391d705f1005aef6cc
Ilija Vukotic <ivukotic@cern.ch>
2013-11-10 15:30:44 -0600

    smaller fixes.

--- SSSserver/src/main/java/edu/uchicago/SSSserver/RootFile.java
+++ SSSserver/src/main/java/edu/uchicago/SSSserver/RootFile.java
@@ -17,12 +17,13 @@ public class RootFile {
 	private ArrayList<tree> trees;
 	boolean started;
 	boolean done;
-	
+
 	RootFile(String na, String gu, long si) {
 		name = na;
 		guid = gu;
 		size = si;
 		logger.info("added file: " + toString());
+		gLFN="";
 		trees = new ArrayList<tree>();
 		started = false;
 		done = false;
@@ -37,21 +38,22 @@ public class RootFile {
 		return trees;
 	}
 
-	public Long getEventsInTree(String treename){
-		for (tree t:trees){
-			if (t.getName().equals(treename)) return t.getEvents();
+	public Long getEventsInTree(String treename) {
+		for (tree t : trees) {
+			if (t.getName().equals(treename))
+				return t.getEvents();
 		}
 		return 0L;
 	}
-	
+
 	public void setGLFN(String glfn) {
 		gLFN = glfn;
 	}
 
-	public String getFullgLFN(){
-		return gLFN+"/"+name;
+	public String getFullgLFN() {
+		return gLFN + "/" + name;
 	}
-	
+
 	private class Inspector extends Thread {
 
 		public void run() {
@@ -59,24 +61,28 @@ public class RootFile {
 				if (gLFN.equalsIgnoreCase(""))
 					return;
 				trees.clear();
-				Runtime rt = Runtime.getRuntime();
-				String comm = "./inspector " + gLFN + "/" + name;
-				logger.info("executing >" + comm + "<");
-				Process pr = rt.exec(comm);
-				BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-
+				logger.info("Inspecting  >" + gLFN + "/" + name + "<");
+				Process p = new ProcessBuilder("./inspector", gLFN + "/" + name).start();
+				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+				
+				while (!input.ready() && !error.ready())
+					sleep(1000);
+				logger.error(error.readLine());
+				
 				String line;
-				Integer nt=-1;
+
+				Integer nt = -1;
 				while ((line = input.readLine()) != null) {
-					logger.debug(line);
+					logger.info(line);
 					if (line.startsWith("ntrees:")) {
-						nt=Integer.parseInt(line.substring(7));
+						nt = Integer.parseInt(line.substring(7));
 						break;
 					}
 				}
-				if (nt==-1){
+				if (nt == -1) {
 					logger.error("problem: should have returned number of trees in file.");
-					started=false;
+					started = false;
 					return;
 				} else {
 					for (int i = 0; i < nt; i++) {
@@ -102,9 +108,8 @@ public class RootFile {
 							String[] br = line.split("\t");
 							if (br.length == 2) {
 								toa.addBranch(br[0], Long.parseLong(br[1]));
-							}
-							else {
-								logger.error("problematic line"+line);
+							} else {
+								logger.error("problematic line" + line);
 							}
 						}
 						logger.info(toa.toString());
@@ -119,13 +124,14 @@ public class RootFile {
 				started = false;
 				done = false;
 			}
-			done=true;
-			
+			done = true;
+
 		}
 
 	}
 
 	public String toString() {
-		return "ROOT file: " + name + "\t bytes: " + size;// + "\tGUID: " + guid;
+		return "ROOT file: " + name + "\t bytes: " + size;// + "\tGUID: " +
+															// guid;
 	}
 }
